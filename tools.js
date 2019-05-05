2ca9ccf56fc760e182141973f510b2c0a3f8de80
Muricans <muricansisphat@gmail.com>
2019-05-05 00:23:46 -0500

    link

--- tools.js
+++ tools.js
@@ -126,8 +126,9 @@ module.exports.getImage = function (apiURL, apiKey, message) {
   let postData = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; ------WebKitFormBoundary7MA4YWxkTrZu0gW--";
   req.setHeader('content-type', 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW');
 
-  req.write(postData.link);
-  message.channel.send(req.write(postData.link));
+  req.each(postData, function (image) {
+    message.channel.send(image.link);
+  });
 
   req.end();
 }
