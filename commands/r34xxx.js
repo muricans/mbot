60572d16be8043099aca461be9c4a70778ae13b9
Muricans <muricansisphat@gmail.com>
2019-05-06 16:28:33 -0500

    Made some mistakes forsenCD

--- commands/r34xxx.js
+++ commands/r34xxx.js
@@ -3,7 +3,7 @@ const tools = require('../tools.js');
 module.exports = {
   name: 'r34xxx',
   execute(message, args) {
-    message.delete(500);
+    message.delete(1000);
     if (!message.channel.nsfw) {
       return message.channel.send("Please move to an nsfw channel :flushed:");
     }
@@ -11,6 +11,6 @@ module.exports = {
       return tools.rule34(message, false);
     }
     let searchArgs = args.join('+');
-    return tools.rule34Tags(message, true, searchArgs);
+    return tools.rule34(message, true, searchArgs);
   },
 };
