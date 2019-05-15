584192b8f225d1d18b3dc6c0847e356b477a5be4
Josh <muricansisphat@gmail.com>
2019-05-15 01:23:37 -0500

    yes

--- commands/suggestions.js
+++ commands/suggestions.js
@@ -1,12 +1,25 @@
 const fs = require('fs');
-const Discord = require('discord.js');
 
 module.exports = {
     name: 'suggestions',
+    usage: '[clear]',
     execute(message, args) {
-        const suggestions = JSON.parse(fs.readFileSync('./suggestions.json', 'utf8'));
-        for (var i in suggestions) {
-            message.channel.send(suggestions[i].suggestion + '\n' + suggestions[i].by);
+        if (args.length === 0) {
+            const suggestions = JSON.parse(fs.readFileSync('./suggestions.json', 'utf8'));
+            for (var i in suggestions) {
+                message.channel.send(suggestions[i].suggestion + '\n' + suggestions[i].by);
+            }
+            return;
+        }
+        if (args[0] === "clear") {
+            let hasAdmin = message.channel.permissionsFor(message.author).has("ADMINISTRATOR");
+            if (!hasAdmin) return message.channel.send(message.author('You do not have permission to use this command!'));
+            return fs.writeFile('./suggestions.json', JSON.stringify([]), (err) => {
+                if (err) console.log(err);
+                message.channel.send('Cleared suggestions list.');
+            })
+        } else {
+            return message.channel.send('Invalid usage! !suggestions [clear]');
         }
     },
 };
