defb5c4867d6b5c71c752ee027d752014c788445
Josh <muricansisphat@gmail.com>
2019-05-15 01:09:33 -0500

    maybe?

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,12 +3,15 @@ const fs = require('fs');
 module.exports = {
     name: 'suggest',
     execute(message, args) {
+        if (args.length === 0) {
+            return message.reply('Please add params! !suggest <suggestion>');
+        }
         const suggestions = JSON.parse(fs.readFileSync('./suggestions.json', 'utf8'));
         suggestions.push({
             "suggestion": args.join(' '),
             "by": message.author.username
         });
-        fs.writeFile('./suggestions.json', 'utf8', (err) => {
+        fs.writeFile('./suggestions.json', JSON.stringify(suggestions), (err) => {
             if (err) console.log(err);
         });
     },
