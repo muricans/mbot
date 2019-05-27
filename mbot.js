9a2627a2f70b420b79ca851063897cc4c05069bb
Josh <muricansisphat@gmail.com>
2019-05-27 12:50:02 -0500

    remove express since it's useless

--- mbot.js
+++ mbot.js
@@ -6,17 +6,12 @@ const tools = require('./tools.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const sqlite = require('sqlite3').verbose();
-const express = require('express');
-const fs = require('fs');
-const app = express();
 const Logger = require('./logger');
 
 if (settings.token === "YOURTOKEN") {
   Logger.error('Please add your token to the bot!');
   return process.exit(1);
 }
-
-app.use(express.json());
 
 /**
  * This bots EventEmitter
@@ -252,13 +247,5 @@ event.on('editCommand', (command, msg) => {
 
 commands.registerCommands(client, this);
 
-app.get('/suggestions', (req, res) => {
-  fs.createReadStream('./suggestions.json', 'utf8').on('data', (chunk) => {
-    let suggestions = JSON.parse(chunk);
-    res.send(suggestions);
-  });
-});
-
-//app.listen(80);
 //login to the client
 client.login(settings.token);
