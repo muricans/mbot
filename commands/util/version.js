690c6544fadafcfe3972f080faa30fc1b49b97d1
Josh <muricansisphat@gmail.com>
2019-06-02 13:15:13 -0500

    version command update

--- commands/util/version.js
+++ commands/util/version.js
@@ -1,5 +1,6 @@
 const pkg = require('../../package.json');
 const Discord = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
     name: 'version',
@@ -7,14 +8,18 @@ module.exports = {
     description: 'Gets the current version of the bot',
     cooldown: 3,
     execute(message) {
-        const embed = new Discord.RichEmbed()
-            .setTitle('mbot info')
-            .setThumbnail('https://hotemoji.com/images/dl/v/robot-face-emoji-by-twitter.png')
-            .setColor('#e9225f')
-            .addField('Version', `v${pkg.version}`, true)
-            .addField('Author', 'muricans', true)
-            .addField('Repository', '[GitHub](https://github.com/muricans/mbot)', true)
-            .addField('Documentation', '[Here](https://muricans.github.io/)', true);
-        message.channel.send(embed);
+        fs.readFile('./version.txt', 'utf8', (err, data) => {
+            if (err) return console.log(err);
+            const embed = new Discord.RichEmbed()
+                .setTitle('mbot info')
+                .setThumbnail('https://hotemoji.com/images/dl/v/robot-face-emoji-by-twitter.png')
+                .setColor('#e9225f')
+                .addField('Version', `v${pkg.version}`, true)
+                .addField('Author', 'muricans', true)
+                .addField('Repository', '[GitHub](https://github.com/muricans/mbot)', true)
+                .addField('Documentation', '[Here](https://muricans.github.io/)', true)
+                .setFooter(`Git commit: ${data}`, 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png');
+            message.channel.send(embed);
+        });
     },
 };
