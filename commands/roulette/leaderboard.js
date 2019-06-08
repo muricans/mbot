06d4910f1d3ba30fe264376e7246d4aac90e197e
Josh <muricansisphat@gmail.com>
2019-06-08 13:42:31 -0500

    maybe?

--- commands/roulette/leaderboard.js
+++ commands/roulette/leaderboard.js
@@ -16,17 +16,29 @@ module.exports = {
 
 function leaderboard(message, client) {
     return new Promise((resolve) => {
-        db.all('SELECT points points, id id FROM users ORDER BY points DESC', (err, rows) => {
+        db.all('SELECT points points, id id FROM users ORDER BY points DESC', async (err, rows) => {
             if (err) return console.log(err);
             const embed = new Discord.RichEmbed().setTitle('Points Leaderboard');
             if (!rows.length) return message.channel.send('No users found!');
-            rows.map(async (val, i, arr) => {
-                if (i < 20) {
-                    const user = await client.fetchUser(arr[i].id);
-                    embed.addField(`${i + 1}. ${user.username}`, arr[i].points, true);
+            const each = new Promise(async (resolve) => {
+                const users = [];
+                rows.forEach((val, i, arr) => {
+                    users.push({
+                        id: arr[i].id,
+                        points: arr[i].points,
+                    });
+                });
+                return resolve(users);
+            });
+            await each.then(async users => {
+                for (let i = 0; i < 20; i++) {
+                    if (users[i]) {
+                        const user = await client.fetchUser(users[i].id);
+                        embed.addField(`${i + 1}. ${user.username}`, users[i].points, true);
+                    }
                 }
             });
-            resolve(embed);
+            return resolve(embed);
         });
     });
 }
