ca846c391e04352524a67eac934e446f5edda32a
muricans <muricansisphat@gmail.com>
2019-12-08 22:47:56 -0600

    purge command to leave servers

---
+++ commands/util/purge.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'purge',
+    usage: 'No usage data found.',
+    description: 'Purge all servers mbot is connected to except the server this command is being executed from.',
+    owner: true,
+    execute(message, args, client) {
+        for (const i in client.guilds.array()) {
+            const guild = client.guilds.array()[i];
+            if (guild == message.guild) continue;
+            guild.leave();
+        }
+    },
+};
