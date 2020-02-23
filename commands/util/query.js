bebf61a0803a99f024009b749d7166a1925d44e9
muricans <muricansisphat@gmail.com>
2020-02-23 02:01:50 -0600

    Add option to force full query
    Add $full before your query when using the query command to force a full response

--- commands/util/query.js
+++ commands/util/query.js
@@ -5,27 +5,37 @@ const waAPI = WolframAlphaAPI(key);
 
 module.exports = {
     name: 'query',
-    usage: '<query>',
-    description: 'Query information by utilizing Wolfram',
+    usage: '[$full] <query>',
+    description: 'Query information by utilizing Wolfram | Add $full before query to force a full response',
     args: true,
     minArgs: 1,
     execute(message, args) {
-        const query = args.slice(0, args.length).join(' ');
-        waAPI.getShort(query).then(result => message.channel.send(result))
-            .catch(err => {
-                if (err.toString().endsWith('No short answer available'))
-                    waAPI.getFull(query).then(result => {
-                        const interpreation = result.pods[0].subpods[0].plaintext;
-                        const description = result.pods[1].subpods[0].plaintext.substring(0, 1777);
-                        message.channel.send(`Interpreted: ${interpreation}\n${description}`);
-                    }).catch(err => {
-                        if (err.toString().endsWith(`Cannot read property '0' of undefined`))
-                            message.channel.send('No data could be interpreted');
-                        else
-                            message.channel.send(`${err}`);
-                    });
-                else
-                    message.channel.send(`${err}`);
-            });
+        if (args[0] == '$full' && args.length > 1)
+            getFull(args.slice(1, args.length).join(' ')).then(result => message.channel.send(result));
+        else {
+            const query = args.join(' ');
+            waAPI.getShort(query).then(result => message.channel.send(result))
+                .catch(err => {
+                    if (err.toString().endsWith('No short answer available'))
+                        getFull(query).then(result => message.channel.send(result));
+                    else
+                        message.channel.send(`${err}`);
+                });
+        }
     },
 };
+
+function getFull(query) {
+    return new Promise(resolve => {
+        waAPI.getFull(query).then(result => {
+            const interp = result.pods[0].subpods[0].plaintext;
+            const desc = result.pods[1].subpods[0].plaintext.substring(0, 1777);
+            resolve(`Interpreted: ${interp}\n${desc}`);
+        }).catch(err => {
+            if (err.toString().endsWith(`Cannot read property '0' of undefined`))
+                resolve('No data could be interpreted');
+            else
+                resolve(`${err}`);
+        });
+    });
+}
